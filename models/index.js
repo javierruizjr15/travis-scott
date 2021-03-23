8c18053adb2ab705a2c4f8c54cd2e7b08f3a1000
Yafiet Isaac <yafietisaac@msn.com>
2021-03-23 15:20:51 -0700

    :updated generalblog routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const User = require('./User.js')
 const musicBlog = require('./musicBlog.js')
 const merchBlog = require('./merchBlog.js')
 const foodBevBlog = require('./foodBevBlog.js')
+const generalBlog = require ('./generalBlog.js')
 
 User.hasMany(musicBlog, { foreignKey: 'uid' })
 musicBlog.belongsTo(User, { foreignKey: 'uid' })
@@ -14,4 +15,4 @@ foodBevBlog.belongsTo(User, { foreignKey: 'uid' })
 
 
 
-module.exports = { User, musicBlog, merchBlog, foodBevBlog }
+module.exports = { User, musicBlog, merchBlog, foodBevBlog, generalBlog }
