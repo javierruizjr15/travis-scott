d4a3a7e1e9d60f66c0a9ddd5673077d53db730c2
Javier Ruiz <javierruizjr15@gmail.com>
2021-03-24 10:54:27 -0700

    yes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
 const router = require('express').Router()
 
-<<<<<<< HEAD
-router.use('/', require('./viewRoutes.js'))
-router.use('/', require('./viewRoutes.js'))
-router.use('/', require('./viewRoutes.js'))
-
-module.exports = router
-=======
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./musicBlogRoutes.js'))
 router.use('/seed', require('./seedRoutes.js'))
@@ -15,4 +8,3 @@ router.use('/api',require('./merchBlogRoutes.js'))
 router.use('/api', require('./generalBlogRoutes.js'))
 
 module.exports = router
->>>>>>> cdbfde12f64396153686e084169fdd443f0dbdd3
