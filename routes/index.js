eb906af810b59bf1bb74d14b72ec2ae26023080a
Javier Ruiz <javierruizjr15@gmail.com>
2021-03-24 10:03:57 -0700

    lotr

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+
+router.use('/', require('./viewRoutes.js'))
+router.use('/', require('./viewRoutes.js'))
+router.use('/', require('./viewRoutes.js'))
+
+module.exports = router
